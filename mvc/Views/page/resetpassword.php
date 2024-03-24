c34a9a02efafae85425958a4ce3de3f0969e467f
Mỹ Ly <114343103+Myyly@users.noreply.github.com>
2024-03-24 16:16:46 +0700

    update

--- mvc/Views/page/resetpassword.php
+++ mvc/Views/page/resetpassword.php
@@ -5,11 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reset Password</title>
-    <!-- Import thư viện Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="/assets/css/resetpassword.css"> <!-- Đảm bảo rằng bạn có file CSS cho trang reset password -->
+    <link rel="stylesheet" href="/assets/css/resetpassword.css">
 </head>
-
 <body>
     <div class="resetPasswordBox">
         <h3>Reset Password</h3>
---
+++ mvc/Views/page/updatepassword.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Đổi mật khẩu</title>
+    <link rel="stylesheet" href="/assets/css/updatepassword.css">
+</head>
+
+<body>
+    <div class="changePasswordBox">
+        <h3>Đổi mật khẩu</h3>
+        <form action="" method="post">
+            <div class="inputBox">
+                <input id="oldPassword" type="password" name="oldPassword" placeholder="Mật khẩu cũ">
+            </div>
+            <div class="inputBox">
+                <input id="newPassword" type="password" name="newPassword" placeholder="Mật khẩu mới">
+            </div>
+            <div class="inputBox">
+                <input id="confirmNewPassword" type="password" name="confirmNewPassword" placeholder="Xác nhận mật khẩu mới">
+            </div>
+            <input type="submit" value="Đổi mật khẩu">
+        </form>
+    </div>
+</body>
+
+</html>
